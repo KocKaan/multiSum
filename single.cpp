e0f3218a23142ef50756c05ef34b0f9d162f59ad
KocKaan <kk4291@nyu.edu>
2022-11-23 03:02:38 -0500

    Wrote with 1 thread

--- single.cpp
+++ single.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+int sum(const vector<int> &arr)
+{
+
+    int counter = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        counter += arr[i];
+    }
+
+    return counter;
+}
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    cout << "Hello World" << endl;
+
+    vector<int> vect = {1, 10, 13, 43, 43, 543, 63};
+
+    int result = sum(vect);
+
+    cout << result << endl;
 }
